4d3c3139594b84bfe2917b4eae03fc9fbabcf79a
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-23 19:10:43 -0400

    blue

--- pokemon/src/components/Card.jsx
+++ pokemon/src/components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ pokemon, loading,infoPokemon}) => {
 <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
 <h2>{item.id}</h2>
     <img src={item.sprites.front_default} alt="" />
-    <img className='card' src="https://images.wombo.art/generated/29e72828-4f9e-41fc-8864-6550ad3259be/final.jpg?Expires=1689431182&Signature=STnASdcykmmdccovmfiMgVpJ5IKbJ8rdWrqOedzopCpt7zP-CHqr5GzttQ~Hdps05zx0Hovg~PLmbcBa9fvzh0oFhd6SkwZUUtDF0-wtnEXHiNk7TD8fUex5rxowQHYjwBmgmIbAxWaUKBuowZzyyVogaJQsqYfyOuBhF4hs1nptHfatZcP~scgAm9GuZiaoR~KNrvR-dVemS8oclTHMvwgkvwS2dlNqP-WPP~~RtCjmg5bMS~pG5T-G2Ag9-3njZPvAcT6kaUmBMCbDnJhaY2eXtI3pNEBwVuRpHrULoe6KCJHBobWphfo~txsSEbYOmSe30WB5VDqpNImcEMKnWQ__&Key-Pair-Id=K1ZXCNMC55M2IL" alt="" />
+    <img className='card' src="https://images.wombo.art/generated/feb4d43e-2f26-4974-bfbf-a413da47d934/final.jpg?Expires=1689490902&Signature=S-qg7DEo0d1JSqs8fgUqSXMyD-cb9CUAHt2BJzrObeaaoMPgNb0Uv~ize~Z3xHcQlcx7Fwgs-BsGrcyVWOjDtxZ1q9YOo-EP0jM~8o9E5kWciFq9UikPzX2v8DRydfFVEF8OE4wQBwowsSdbq-N~nsL0-oGZ4PBGLWFpCMy8c0qKArONAIO2DZ3dDgK66mdOq5ohNMVud54Yv8maGEJxJcZDdynx5Y5kSxAOUUe7H2w0MS2WeLElTvbD0AvN~wYfS6kfFU2z3xOEfxy44RB3Y4WYA~0JAci7FtjcQNVmakT5KjZq2-eNMVn7Pw655wWCi~sd-ZKPH4nU8RSFoILWcg__&Key-Pair-Id=K1ZXCNMC55M2IL" alt="" />
     <h2>{item.name}</h2>
 </div>
 </>
